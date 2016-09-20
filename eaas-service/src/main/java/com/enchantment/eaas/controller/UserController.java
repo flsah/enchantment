048ed3b3bd42cea746567411573448a6adde50e1
liushuang <flsah.cn@gmail.com>
2016-09-20 16:56:55 +0800

    Add user controller methods

--- eaas-service/src/main/java/com/enchantment/eaas/controller/UserController.java
+++ eaas-service/src/main/java/com/enchantment/eaas/controller/UserController.java
@@ -9,14 +9,26 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
+ * The controller which to handle the request about user entity.<br/>
+ *
  * Created by liushuang on 2016/9/19.
  */
 @RestController
-@RequestMapping(value = "/user", produces = "application/json")
+@RequestMapping(value = "/v1/user", produces = "application/json")
 public class UserController {
-    @Autowired
     private UserService service;
 
+    @Autowired
+    public UserController(UserService service) {
+        this.service = service;
+    }
+
+    /**
+     * Select one user information by user id
+     *
+     * @param id user identity
+     * @return user information
+     */
     @RequestMapping(value = "/{id}",
             method = {RequestMethod.POST, RequestMethod.GET})
     public User getUser(@PathVariable("id") String id) {
@@ -32,5 +44,41 @@ public class UserController {
     @RequestMapping(value = "/auth", method = RequestMethod.POST)
     public boolean auth(User user) {
         return service.login(user);
+    }
+
+    /**
+     * Add one user into db
+     * <p>Request method is <code>PUT</code></p>
+     *
+     * @param user new user information
+     * @return true if add successfully, otherwise false
+     */
+    @RequestMapping(value = "/add", method = RequestMethod.PUT)
+    public boolean useradd(User user) {
+        return service.addUser(user);
+    }
+
+    /**
+     * Delete one user from db
+     * <p>Request method is <code>DELETE</code></p>
+     *
+     * @param id user identity to be deleted
+     * @return true if delete successfully, otherwise false
+     */
+    @RequestMapping(value = "/del/{id}", method = RequestMethod.DELETE)
+    public boolean userdel(@PathVariable("id") String id) {
+        return service.removeUser(id);
+    }
+
+    /**
+     * Update one user information
+     * <p>Request method is <code>PUT</code></p>
+     *
+     * @param user new user information for changing
+     * @return true if modify successfully, otherwise false
+     */
+    @RequestMapping(value = "/mod", method = RequestMethod.PUT)
+    public boolean usermod(User user) {
+        return service.updateUser(user);
     }
 }
