ba5214c1107a93c745ae92284e556f13407f43f3
lius <flsah.cn@gmail.com>
2016-09-24 12:21:59 +0800

    Modify eaas module

--- eaas-service/src/main/java/com/enchantment/eaas/service/UserService.java
+++ eaas-service/src/main/java/com/enchantment/eaas/service/UserService.java
@@ -1,22 +1,18 @@
 package com.enchantment.eaas.service;
 
-import static com.enchantment.eaas.Constants.*;
-import static java.util.stream.Collectors.toList;
-
-import com.alibaba.druid.support.json.JSONUtils;
 import com.enchantment.eaas.CacheUtil;
 import com.enchantment.eaas.Utils;
 import com.enchantment.eaas.dao.RoleDAO;
 import com.enchantment.eaas.dao.UserDAO;
-import com.enchantment.eaas.domain.Role;
 import com.enchantment.eaas.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.servlet.http.HttpSession;
 import java.sql.Timestamp;
-import java.util.ArrayList;
 import java.util.List;
+
+import static com.enchantment.eaas.Constants.OPER_N;
+import static com.enchantment.eaas.Constants.STAT_INIT;
 
 /**
  * <p>User operation service</p>
@@ -39,33 +35,6 @@ public class UserService {
 
     public User auth(User user) {
         return userDAO.getUser(user);
-    }
-
-    public boolean login(User user, HttpSession session) {
-        User reUser = auth(user);
-
-        if (reUser == null)
-            return false;
-
-        session.setAttribute(TOKEN_CUID, user.getId());
-
-        cache.push(user.getId(), JSONUtils.toJSONString(reUser));
-
-        List<Role> roles = roleDAO.getRolesByUser(user.getId());
-        List<String> strR = new ArrayList<>(roles.size());
-        roles.stream().map(role -> JSONUtils.toJSONString(role)).collect(toList());
-
-        return true;
-    }
-
-    public boolean logout(HttpSession session) {
-        Object cuid = session.getAttribute(TOKEN_CUID);
-        if (cuid == null)
-            return true;
-
-//        cache
-
-        return true;
     }
 
     public User getUser(String id) {
